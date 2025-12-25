24201f444dc2675c2c90170f2c090d1adda1d8c4
Pranav Abhimanyu <dpranav503@gmail.com>
2025-12-25 23:30:16 +0530

    auth callback added

--- app/(auth)/auth/callback/page.jsx
+++ app/(auth)/auth/callback/page.jsx
@@ -1,14 +1,9 @@
 import { redirect } from "next/navigation"
-
 export default async function AuthCallbackPage() {
-  console.log("🔥 AUTH CALLBACK HIT")
-
   const baseUrl =
     process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000"
-
   await fetch(`${baseUrl}/api/user/sync`, {
     method: "POST",
   })
-
   redirect("/")
 }
